5177911c0b164c365d09d95075a40e2fee74fac4
happy663 <53854298+happy663@users.noreply.github.com>
2021-11-03 19:18:41 +0900

    resのエンドポイントの変更

--- go/main.go
+++ go/main.go
@@ -6,4 +6,40 @@ import (
 
 func main() {
 	fmt.Println("hello")
+	arr := Input()
+	fmt.Println(arr)
+
+}
+
+//クイックソートを配列で返す
+func QuickSort(arr []int) []int {
+	if len(arr) < 2 {
+		return arr
+	}
+
+	pivot := arr[0]
+	var left, right []int
+	for _, v := range arr[1:] {
+		if v <= pivot {
+			left = append(left, v)
+
+		} else {
+			right = append(right, v)
+		}
+	}
+
+	return append(append(QuickSort(left), pivot), QuickSort(right)...)
+
+}
+
+//スペースで区切られた整数の入力
+func Input() []int {
+	var n, tmp int
+	fmt.Scan(&n)
+	arr := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&tmp)
+		arr[i] = tmp
+	}
+	return arr
 }
