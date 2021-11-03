84417554b38e45a73b820fb47a70ca1ffae88912
wkympand0 <ari7ri2it1018@gmail.com>
2021-11-03 23:08:42 +0900

    frontenv

---
+++ front/components/button.tsx
@@ -0,0 +1,9 @@
+const button = () => {
+  return (
+    <div>
+      <h1>ボタン</h1>
+    </div>
+  );
+};
+
+export default button;
---
+++ front/pages/test.tsx
@@ -0,0 +1,12 @@
+import Button from "../components/button";
+
+const test = () => {
+  return (
+    <div>
+      <h1>test</h1>
+      <Button></Button>
+    </div>
+  );
+};
+
+export default test;
